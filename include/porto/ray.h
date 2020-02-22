352b72562b587499eac837d45ffd941592bcbec0
ivan <kudriakov.iv@phystech.edu>
2020-02-22 14:35:59 +0300

    update ray.h

--- include/porto/ray.h
+++ include/porto/ray.h
@@ -1,10 +1,19 @@
 #pragma once
-#include <porto/vec3.h>
+//#include <porto/vec3.h>
+#include "vec3.h"
 
 namespace porto
 {
     class Ray
     {
-        
+        Vec3 src, dir;
+    public:
+        Ray(Vec3 source, Vec3 direction) : src{source}, dir{direction} {
+            dir.normalize();
+        }
+        Vec3 src() { return src; }
+        Vec3 dir() { return dir; } 
+        Vec3 point_at_parameter(float t) { return src + t*dir; }
     }
 }
+
